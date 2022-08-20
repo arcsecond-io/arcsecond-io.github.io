0906cceee075a22c0662cabf36d285e774953dac
Cédric Foellmi <cedric@onekiloparsec.dev>
2022-08-20 10:56:21 +0200

    Fixed config
    Signed-off-by: Cédric Foellmi <cedric@onekiloparsec.dev>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,10 @@
 export default {
+  title: 'Arcsecond Docs',
+  description: 'A Platform for Astronomers and Observatories',
+  siteTitle: false,
+  cleanUrls: 'with-subfolders',
   themeConfig: {
     logo: '/logo-circle-sm.png',
-    title: 'Arcsecond Docs',
-    description: 'A Platform for Astronomers and Observatories',
-    siteTitle: false,
-    cleanUrls: 'with-subfolders',
     nav: [
       { text: 'iObserve & Pro Tools', link: '/iobserve/introduction' },
       { text: 'Observatory Portals', link: '/portals/introduction' },
